32b5119dc49a9de2339c9e671d42aad3540069cf
rbvwhyry <51495041+rbvwhyry@users.noreply.github.com>
2024-10-21 00:49:30 -0700

    Update platform.py

--- lib/platform.py
+++ lib/platform.py
@@ -136,7 +136,7 @@ class PlatformRasp:
         hotspot_config_content = """
 interface=wlan0
 driver=nl80211
-ssid=PianoLEDVisualizer
+ssid=key2play
 hw_mode=g
 channel=7
 wmm_enabled=0
@@ -144,7 +144,7 @@ macaddr_acl=0
 auth_algs=1
 ignore_broadcast_ssid=0
 wpa=2
-wpa_passphrase=visualizer
+wpa_passphrase=key2play
 wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
 rsn_pairwise=CCMP
