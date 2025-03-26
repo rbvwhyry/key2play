330b59578ce2790ff07f3937111008afa7d1d3c2
rbvwhyry <51495041+rbvwhyry@users.noreply.github.com>
2025-03-25 23:33:27 -0700

    Update views_api.py
    bright to 216

--- webinterface/views_api.py
+++ webinterface/views_api.py
@@ -126,7 +126,7 @@ def set_all_lights():
     cntLed = webinterface.appconfig.num_leds_on_strip()
     for i in range(cntLed):
         strip.setPixelColor(i, color)
-    strip.setBrightness(208)
+    strip.setBrightness(216)
     strip.show()
     return jsonify(success=True)
 
