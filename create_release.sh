314cb57ad3a1d37077ab65ed0d5de200148542c3
aaronmrice <372839+aaronmrice@users.noreply.github.com>
2025-09-15 19:12:30 -0700

    fix shellcheck complaint in create_release

--- create_release.sh
+++ create_release.sh
@@ -12,6 +12,6 @@ HASH="$(sha256sum "${FILELIST[@]}" | sha256sum)"
 HASH_PREFIX="${HASH:0:10}"
 DATE="$(date -u '+%Y_%m_%d_%H_%M_%S')"
 ZIPFILE_NAME="release_${DATE}_${HASH_PREFIX}.zip"
-zip "${ZIPFILE_NAME}" ${FILELIST[@]}
+zip "${ZIPFILE_NAME}" "${FILELIST[@]}"
 cp "${ZIPFILE_NAME}" gh-pages/
 jq ".[.| length] |= . + \"${ZIPFILE_NAME}\"" gh-pages/releases.json | sponge gh-pages/releases.json
