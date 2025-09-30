2d64b4bb7f799b33e86d4ad722f91ab865b1bcd9
aaronmrice <372839+aaronmrice@users.noreply.github.com>
2025-09-30 16:05:00 -0700

    improve create_release.sh

--- create_release.sh
+++ create_release.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -exo pipefail
+set -e # this option quits the file if a process returns a non-zero exit code
+set -o pipefail # sets the exit code of a pipeline to the rightmost non-zero exit code
+set -u # treat unset variables as an error
+set -x # print the commands being executed
 
 shopt -s nullglob
+
+# create gh-pages directory if it doesn't already exist
+if [ -d gh-pages ]; then
+    git worktree add ./gh-pages gh-pages
+fi
 
 FILELIST=(./*.py ./*.sh ./**/*.py ./crazy-sky-250204/*.* ./crazy-sky-250204/src/*.* ./crazy-sky-250204/src/static/**/*.*)
 
