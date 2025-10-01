18f82ccbe780a98559834614d4406bef169ab3fa
aaronmrice <372839+aaronmrice@users.noreply.github.com>
2025-10-01 04:13:18 -0700

    fix outdated entry in sources.list

--- autoinstall.sh
+++ autoinstall.sh
@@ -57,7 +57,7 @@ add_sources() {
   cat <<EOF | sudo tee "/etc/apt/sources.list.d/debian-bullseye.list"
   deb http://deb.debian.org/debian bullseye main contrib non-free
   deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-  deb http://deb.debian.org/debian bullseye-backports main contrib non-free
+  deb http://archive.debian.org/debian bullseye-backports main contrib non-free
   deb http://security.debian.org/debian-security/ bullseye-security main contrib non-free
 EOF
 }
