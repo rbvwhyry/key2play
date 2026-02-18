0af4391af1543c6ed67ee82fe4b197ed964df0fd
aaronmrice <372839+aaronmrice@users.noreply.github.com>
2026-02-17 22:13:34 -0800

    move map.py code to config.py
    In order for the application to have ACID properties WRT key2play.sqlite,
    we wrap all accesses in a brand new sqlalchemy Engine. This is probably overkill,
    but it's effective.
    The recreation of schemas during Config::backup_config_file_and_reset_to_factory
    demands all sqlalchemy types to be in this file (or maybe imported into this file?)

--- config.py
+++ config.py
@@ -110,3 +110,101 @@ class Config:
 
     def set_is_hotspot_active(self, val: bool):
         self.set_config("is_hotspot_active", val)
+
+
+class MidiLedMap(Base):
+    __tablename__ = "midi_led_map"
+    midi_note: Mapped[int] = mapped_column(primary_key=True)
+    led_index: Mapped[int] = mapped_column()
+    r: Mapped[int] = mapped_column()
+    g: Mapped[int] = mapped_column()
+    b: Mapped[int] = mapped_column()
+    time_on: Mapped[int] = mapped_column()
+    time_off: Mapped[int] = mapped_column()
+
+    def __repr__(self) -> str:
+        return (
+            f"MidiLedMap("
+            f"midi_note={self.midi_note}, "
+            f"led_index={self.led_index}, "
+            f"r={self.r}, "
+            f"g={self.g}, "
+            f"b={self.b}, "
+            f"time_on='{self.time_on}', "
+            f"time_off='{self.time_off}'"
+            f")"
+        )
+
+
+class MidiToLedMapping:
+    def __init__(self):
+        self.create_schema()
+
+    # Insert or update a mapping
+    def set_midi_led_row(
+        self,
+        midi_note: int,
+        led_index: int,
+        r: int,
+        g: int,
+        b: int,
+        time_on: int,
+        time_off: int,
+    ):
+        engine = create_engine(CONNECTION_STRING)
+        with Session(engine) as session:
+            stmt = (
+                insert(MidiLedMap)
+                .values(
+                    midi_note=midi_note,
+                    led_index=led_index,
+                    r=r,
+                    g=g,
+                    b=b,
+                    time_on=time_on,
+                    time_off=time_off,
+                )
+                .on_conflict_do_update(
+                    index_elements=["midi_note"],
+                    set_={
+                        "led_index": led_index,
+                        "r": r,
+                        "g": g,
+                        "b": b,
+                        "time_on": time_on,
+                        "time_off": time_off,
+                    },
+                )
+            )
+            session.execute(stmt)
+            session.commit()
+
+    # Get a mapping by midi_note
+    def get_midi_led_row(self, midi_note: int) -> MidiLedMap | None:
+        engine = create_engine(CONNECTION_STRING)
+        with Session(engine) as session:
+            stmt = select(MidiLedMap).where(MidiLedMap.midi_note == midi_note)
+            result = session.scalar(stmt)
+            return result
+
+    # Delete a mapping by midi_note
+    def delete_midi_led_row(self, midi_note: int):
+        engine = create_engine(CONNECTION_STRING)
+        with Session(engine) as session:
+            stmt = delete(MidiLedMap).where(MidiLedMap.midi_note == midi_note)
+            session.execute(stmt)
+            session.commit()
+
+    # get the entire map
+    def get_midi_led_map(self) -> list[MidiLedMap]:
+        engine = create_engine(CONNECTION_STRING)
+        with Session(engine) as session:
+            stmt = select(MidiLedMap)
+            return list(session.scalars(stmt))
+
+    def delete_all_maps(self):
+        engine = create_engine(CONNECTION_STRING)
+        with Session(engine) as session:
+            stmt = delete(MidiLedMap)
+            session.execute(stmt)
+            session.commit()
